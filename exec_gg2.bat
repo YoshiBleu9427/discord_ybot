2e516bc04d05b73b5adac7b01ee9ecd89df03fff
YoshiBleu9427 <nicolas118218@msn.com>
2020-05-02 18:03:30 +0200

    Add alternative config

---
+++ exec_gg2.bat
@@ -0,0 +1,1 @@
+ java -classpath "./target/classes/;../../../.m2/repository/com/github/austinv11/Discord4j/2.9.3/Discord4j-2.9.3-shaded.jar;../../../.m2/repository/com/google/code/gson/gson/2.8.5/gson-2.8.5.jar;../../../.m2/repository/commons-cli/commons-cli/1.4/commons-cli-1.4.jar" -Dfile.encoding=UTF-8 fr.yb.discordybot.Launcher --config-file gg2bot_config.json
